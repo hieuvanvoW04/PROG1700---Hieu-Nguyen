ebd73060aea603fcb4338755694d0a2ae65a0dc2
Nguyen <W0424530@campus.nscc.ca>
2018-09-11 10:23:09 -0300

    Yes

---
+++ Assignment/In Class/main.py
@@ -0,0 +1,13 @@
+Hello World
+Submit due
+__name__ Hieu Nguyen
+id : W0424530
+_AUTHOR_ = Hieu Nguyen <W0424530@nscc.ca>
+def main():
+    print("Hello World")
+
+
+if __name__=="_main_"
+    main()
+    
+
