649e3635ebeb4012fa0d385263c9037ac4422351
HIEU NGUYEH <43071885+hieuvanvoW04@users.noreply.github.com>
2018-09-12 17:13:53 -0300

    Fix

--- Assignment/In Class/20180905.py
+++ Assignment/In Class/20180905.py
@@ -1,7 +1,9 @@
-"Hello World"
-"Python Push"
-"Name" "Hieu NGuyen", Student  
-ID. W0424530
+"""
+Hello World
+Python Push
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
 _AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
 
 
