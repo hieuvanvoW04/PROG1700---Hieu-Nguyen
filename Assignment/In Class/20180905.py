dde7620e580f554dcd625101ab7d66d5d4963da6
HIEU NGUYEH <43071885+hieuvanvoW04@users.noreply.github.com>
2018-09-12 03:09:17 -0300

    Change

--- Assignment/In Class/20180905.py
+++ Assignment/In Class/20180905.py
@@ -1,13 +1,13 @@
-Hello World
-Submit due
-__name__ Hieu Nguyen
-id : W0424530
-_AUTHOR_ = Hieu Nguyen <W0424530@nscc.ca>
+"Hello World"
+"Python Push"
+"Name" "Hieu NGuyen", Student  
+ID. W0424530
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+
 def main():
     print("Hello World")
 
 
-if __name__=="_main_"
+if __name__=="_main_":
     main()
-    
-
