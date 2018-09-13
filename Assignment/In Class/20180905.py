74094cfe2c685d1fd131268a256a874272ecf39b
HIEU NGUYEH <43071885+hieuvanvoW04@users.noreply.github.com>
2018-09-13 00:55:45 -0300

    Hello World

--- Assignment/In Class/20180905.py
+++ Assignment/In Class/20180905.py
@@ -8,8 +8,8 @@ _AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
 
 
 def main():
-    print("Hello World")
+    print("Hello Prince World")
 
 
-if __name__== "_main_":
+if __name__== "__main__":
     main()
