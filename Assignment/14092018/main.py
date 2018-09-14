4362316479074056e816d5d451d169eebee88d1a
Nguyen <W0424530@campus.nscc.ca>
2018-09-14 11:21:43 -0300

    Cal

--- Assignment/14092018/main.py
+++ Assignment/14092018/main.py
@@ -10,14 +10,16 @@ _AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
 def main():
 
     #Input
-
-
-
+    length=20
+    width=10
+    height=10
 
     #Process
-
-
-
+    
+    wall1= length*height*2
+    wall2=qidth*height*2
+    sqft= wall1+wall2
+    gallon= sqft/150
 
     #Output
     print("You will need (x) gallon")
