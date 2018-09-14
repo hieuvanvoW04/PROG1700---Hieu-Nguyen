3abf2939110fa63f656e032add3a1517d5b104c4
Nguyen <W0424530@campus.nscc.ca>
2018-09-14 10:43:16 -0300

    New

---
+++ Assignment/14092018/main.py
@@ -0,0 +1,15 @@
+"""
+Hello World
+Python Push
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+
+def main():
+    print("Hello Prince World")
+
+
+if __name__== "__main__":
+    main()
