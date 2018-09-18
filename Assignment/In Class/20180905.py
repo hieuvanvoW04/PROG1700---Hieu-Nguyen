2cfccf508635032f17546eaf1807fba9d76ae5ec
Nguyen <W0424530@campus.nscc.ca>
2018-09-18 09:10:57 -0300

    Tech check 2

--- Assignment/In Class/20180905.py
+++ Assignment/In Class/20180905.py
@@ -11,18 +11,18 @@ _AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
 def main():
 
 # Input
-OriginalBill = float( input(" Please enter the original bill: ")
-tip = 0.2 * OriginalBill
-saleTax = 0.15 * OriginalBill
-Totalamount= OriginalBill + tip+ saleTax
+
+OriginalBill = float( input( "Please enter the original bill: " ) )
+
 
 # Process
-
-
-
+tip = 0.2 * OriginalBill
+tax = 0.15 * OriginalBill
+Totalamount = OriginalBill + tip + tax
 
 # Output
-    print( "OriginalBill: $" + format( OriginalBill, ",.2f"))
+    print( "OriginalBill: $"+format( OriginalBill, ",.2f"), "tip: $"+format( tip, ",.2f"),"tax: $"+format(tax, ".,2f"), \
+    "Totalamount: $" + format( Totalamount, ",.2f"), sep = "\n" )
 
 
 if __name__== "__main__":
