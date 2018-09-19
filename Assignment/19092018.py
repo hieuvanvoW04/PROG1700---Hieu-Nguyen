1607945c15df8587a8793ccfdba4f38311a6ca7b
Nguyen <W0424530@campus.nscc.ca>
2018-09-19 12:24:36 -0300

    test

---
+++ Assignment/19092018.py
@@ -0,0 +1,24 @@
+"""
+Hello World
+Python Push
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+import math
+
+def main():
+
+    item="ketchup"
+    reg=1.80   
+    discount=.27
+    price=reg-reg*discount
+
+    temp="${0.2f} is the sale price of {1}"
+    disp=temp.format(price,item)
+
+
+    print(disp)
+if __name__== "__main__":
+    main()
