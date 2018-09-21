ce9822b891a8dc62c9c6a6e8dc47ff936a098086
HIEU NGUYEH <43071885+hieuvanvoW04@users.noreply.github.com>
2018-09-20 21:53:39 -0300

    Ketchup Final

--- Assignment/19092018.py
+++ Assignment/19092018.py
@@ -7,11 +7,11 @@ ID....: W0424530
 _AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
 
 def main():
-    item="ketchup"
+    item= "ketchup"
     reg=1.80
     discount=0.27
     price=reg-reg*discount
-    temp="${0.2f} is the sale price of {1}"
+    temp="${0:.2f} is the sale price of {1}"
     disp=temp.format(price,item)
     print(disp)
 
---
+++ Assignment/Mercury.py
@@ -0,0 +1,17 @@
+"""
+Name..
+: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+
+def main():
+
+
+
+
+
+
+if __name__== "__main__":
+    main()
