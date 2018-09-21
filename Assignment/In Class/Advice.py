ec32cacbfd6a4bea09d43cf3f785de7eb2aa5b14
HIEU NGUYEH <43071885+hieuvanvoW04@users.noreply.github.com>
2018-09-20 23:11:38 -0300

    3 Exercise

---
+++ Assignment/In Class/Advice.py
@@ -0,0 +1,19 @@
+"""
+Name..
+: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+
+def main():
+# Advice
+    
+    prefix="Fore"
+    
+    print("prefix"+"warned", "is","prefix"+"armed")
+
+
+    
+if __name__== "__main__":
+    main()
