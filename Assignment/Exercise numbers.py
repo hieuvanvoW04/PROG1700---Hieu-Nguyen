99ac6939b9e75ac4376cd66c1e8b1d691c4389e3
Nguyen <W0424530@campus.nscc.ca>
2018-09-28 11:38:58 -0300

    In class

---
+++ Assignment/Exercise numbers.py
@@ -0,0 +1,16 @@
+"""
+Hello World
+Python Push
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+def main():
+    deposit=100
+    if deposit>100:
+        print("You get a free toaster")
+print("Have a nice day")
+
+if __name__== "__main__":
+    main()
---
+++ Assignment/If statement.py
@@ -0,0 +1,17 @@
+"""
+Hello World
+Python Push
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+def main():
+   ans=input("Extra Shipping:")
+   ans=ans.lower()
+   if ans=="yes":
+       print("$10")
+print(":)")
+
+if __name__== "__main__":
+    main()
