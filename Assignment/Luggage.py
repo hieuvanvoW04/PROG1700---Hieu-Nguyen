0ccaa04165375952e51f766ef98d281925201c2f
Nguyen <W0424530@campus.nscc.ca>
2018-09-28 12:22:45 -0300

    Inclass2

---
+++ Assignment/Luggage.py
@@ -0,0 +1,18 @@
+"""
+Hello World
+Python Push
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+def main():
+    LuggageWeight=input("Weight of Luggage:")
+    LuggageWeight=float(LuggageWeight)
+    if LuggageWeight>=50:
+        print("$25 surcharge")
+   
+print("Have a nice day")
+
+if __name__== "__main__":
+    main()
