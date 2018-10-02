e25cf0a995ebb26448ea003e119f408783d4fd15
Nguyen <W0424530@campus.nscc.ca>
2018-10-02 09:33:17 -0300

    in class 4.1

---
+++ Assignment/Online shop.py
@@ -0,0 +1,22 @@
+"""
+Hello World
+Python Push
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+def main():
+    country=input("where are you from? ")
+    price= input("Where are the price: ")
+    if(price>2000000 and win=='y'):
+        
+    if country= CANADA:
+        print("")
+    elif country=    
+
+   
+print("Have a nice day")
+
+if __name__== "__main__":
+    main()
