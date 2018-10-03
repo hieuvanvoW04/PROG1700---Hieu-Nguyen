a87670f2c152cf5b9f0351ab333a09bfe1bae97a
HIEU NGUYEH <43071885+hieuvanvoW04@users.noreply.github.com>
2018-10-03 00:28:21 -0300

    check list

--- Assignment/Online shop.py
+++ Assignment/Online shop.py
@@ -1,6 +1,5 @@
 """
-Hello World
-Python Push
+
 Name..: Hieu NGuyen, Student  
 ID....: W0424530
 """
@@ -8,23 +7,18 @@ _AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
 
 def main():
     country=input("where are you from? ")
-    country= country.lower()
-    total= input("What are the order total: ")
+    total=float(input("What are the order total: "))
 
-    if country=="CANADA":
-        province=input("Which province ")
+    if country.lower()=="canada":
+        province=input("Which province? (use Province Code) ")
         if province=="AB":
-            total=total+total*0.05
-            print(total)
-        elif province=="ON"or"NB"or"NS":
-            total=total+total*0.15
-            print(total)
-        elif province!="AB"or"ON"or"NB"or"NS":
-            total=total+total*0.11
-            print(total)
-    else country!="CANADA":
-        print(total)
+            total=total*1.05
+        elif province=="ON"or province=="NB"or province=="NS":
+            total=total*1.15
+        else:
+            total=total*1.11
 
+    print("Your total bill is ${0:.2f}".format(total))
 
 if __name__== "__main__":
     main()
