6833a0ab11f817a652e51e2a022420f6268fd148
Nguyen <W0424530@campus.nscc.ca>
2018-10-05 11:18:13 -0300

    Techcheck3

--- Tech check/Techcheck3.py
+++ Tech check/Techcheck3.py
@@ -7,24 +7,43 @@ _AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
 
 
 def main():
-
-# Input
-
-    EmplyWsalary = float( input( "Please enter the employee's weekly salary: " ) )
-    Perdependent = float( input( "How many dependents do you have: " ) )
-
-
+    print("Valid letter grades that can be enter: A, B, C, D, F.")
+    print("Valid grade modifiers are +, - or nothing.")
+    print("All letter grades except F can include a + or - symbol")
+    print("Calculate grade point value cannot exceed 4.0")
+# Input possible grade are A, B, C, D
+    Grade= input("Please enter a letter grade : ")
+# Input modifiers plus (+), (-) or nothing
+    Mod= input("Please enter a modifier (+, - or nothing): ")
 # Process
-    ProvincialTax = 0.06 * EmplyWsalary
-    FederalTax = 0.25 * EmplyWsalary
-    DependentDeduct= 0.02* Perdependent* EmplyWsalary
-    TotalWithheld= ProvincialTax + FederalTax - DependentDeduct
-    Totaltakehome = EmplyWsalary - TotalWithheld
+    total=0
+    if Grade=="A" and Mod=="+":
+        total=total+4.0
+    elif Grade == "A" and Mod=="":
+        total = total + 4.0
+    elif Grade == "A" and Mode=="-":
+        total = total + 3.7
+    elif Grade == "B" and Mode=="+":
+            total = total + 3.3
+    elif Grade == "B" and Mode=="":
+            total = total + 3.0
+    elif Grade == "B" and Mode=="-":
+            total = total + 2.7
+    elif Grade == "C" and Mode=="+":
+            total = total + 2.3
+    elif Grade == "C" and Mode=="":
+            total = total + 2.0
+    elif Grade == "C" and Mode=="-":
+            total = total + 1.7
+    elif Grade == "D":
+            total = total + 1.0
+    elif Grade=="F":
+            total= total
+    else:
+         print("You enter an invalid letter grade")
 
 # Output
-    print( "EmplyWsalary: $"+format(EmplyWsalary, ",.2f"), "ProvincialTax: $"+format(ProvincialTax, ",.2f"),"FederalTax: $"+format(FederalTax, ".,2f"),"DependentDeduct: $"+format(DependentDeduct, ".,2f"),"TotalWithheld: $"+format(TotalWithheld, ".,2f"), \
-    "Totaltakehome: $" + format( Totaltakehome, ",.2f"), sep = "\n" )
-
+    print("The numeric value is {0:.1f}".format(total))
 
 if __name__== "__main__":
     main()
