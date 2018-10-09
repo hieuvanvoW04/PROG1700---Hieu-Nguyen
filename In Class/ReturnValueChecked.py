145df3da9b6ca74ca5ce045d0d01cb9d92ca3572
Nguyen <W0424530@campus.nscc.ca>
2018-10-09 09:56:14 -0300

    5step

---
+++ In Class/ReturnValueChecked.py
@@ -0,0 +1,16 @@
+"""
+Function
+
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+def square(n):
+    return n * n
+
+def add_one(n):
+    return n + 1
+
+print(square(12))
+print(add_one(square(12)))
