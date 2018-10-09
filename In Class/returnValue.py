6aa04d3d76af9be2e75b41f6990243a0855c83a8
Nguyen <W0424530@campus.nscc.ca>
2018-10-09 09:25:55 -0300

    good

---
+++ In Class/returnValue.py
@@ -0,0 +1,14 @@
+"""
+Function
+
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+def addNumbers(a,b):
+    return(a+b)
+
+    
+sum=addNumbers(199,57)
+print(sum)
