848f3dd601bcad3e208e51f072ad33cade17f896
Nguyen <W0424530@campus.nscc.ca>
2018-10-09 09:38:35 -0300

    3 parametes

---
+++ In Class/1 parametes.py
@@ -0,0 +1,15 @@
+"""
+Function
+
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+def square(x):
+    y=x*x
+    return y
+
+print(square(2))
+
+    
---
+++ In Class/3 parametes.py
@@ -0,0 +1,15 @@
+"""
+Function
+
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+def addEm(x,y,z):
+    return x+y+z
+
+sum=addEm(10,20,30)
+print(sum)
+
+    
