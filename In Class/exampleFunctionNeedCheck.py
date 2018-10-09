7c048d1ca2696e1015bee578239b8500c112bc1b
Nguyen <W0424530@campus.nscc.ca>
2018-10-09 10:15:15 -0300

    CheckReturned

--- In Class/exampleFunctionNeedCheck.py
+++ In Class/exampleFunctionNeedCheck.py
@@ -8,11 +8,13 @@ _AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
 
 def foo():
     print("hello from inside of foo")
-    return __name__
+    return 1
+def bar():
+    return 10* foo()
 
 if __name__== '__main__':
     print("going to call foo")
-    x=foo()
+    x=bar()
     print("call foo")
     print("foo returned "+str(x))
 
