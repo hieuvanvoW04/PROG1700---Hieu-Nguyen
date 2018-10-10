e5e23fd2a4f7461a6621b3f2d8e9b5a57655a98c
Nguyen <W0424530@campus.nscc.ca>
2018-10-10 12:17:09 -0300

    Need check

--- In Class/PaintShop.py
+++ In Class/PaintShop.py
@@ -7,29 +7,17 @@ ID....: W0424530
 _AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
 
 
-import math
-
 square_feet_per_gallon = 150
 
 print("Welcome to the Paint Shop!")
 print("This program will help you calculate how many cans of paint you need to buy, based on the dimensions of your room.")
 
-x=float(input("\nEnter the length of the room, in feet: "))
-y=float(input("Enter the width of the room, in feet: "))
-z=float(input("Enter the height of the room, in feet: "))
+def gallons_of_paint(x,y,z):
+    return ((x*z*2)+(y*z*2))/150
 
-def CalculatetotalArea(x,y,z):
-    
-    return (x*z*2)+(y*z*2)
+gallonsPaint= gallons_of_paint(5,4,3)
 
-totalArea= CalculatetotalArea(x,y,z)
-
-def gallons_of_paint():
-
-    gallons_of_paint= math.ceil(totalArea/square_feet_per_gallon)
-
-print(f"The total wall area of your room is (totalArea)")
-print(f"You need (gallons_of_paint)")
+print(f"You need (gallonsPaint)")
 
 
 
