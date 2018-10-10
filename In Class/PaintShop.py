f8e061764eca796d15b3554ecdcff3353eb92ff7
Nguyen <W0424530@campus.nscc.ca>
2018-10-10 11:54:35 -0300

    Again

--- In Class/PaintShop.py
+++ In Class/PaintShop.py
@@ -11,25 +11,24 @@ import math
 
 square_feet_per_gallon = 150
 
-def hello():
-    print("Welcome to the Paint Shop!")
-    print("This program will help you calculate how many cans of paint you need to buy, based on the dimensions of your room.")
-    return 1
+print("Welcome to the Paint Shop!")
+print("This program will help you calculate how many cans of paint you need to buy, based on the dimensions of your room.")
+
+x=float(input("\nEnter the length of the room, in feet: "))
+y=float(input("Enter the width of the room, in feet: "))
+z=float(input("Enter the height of the room, in feet: "))
 
 def CalculatetotalArea(x,y,z):
-    x=float(input("\nEnter the length of the room, in feet: "))
-    y=float(input("Enter the width of the room, in feet: "))
-    z=float(input("Enter the height of the room, in feet: "))
+    
     return (x*z*2)+(y*z*2)
 
 totalArea= CalculatetotalArea(x,y,z)
 
-print(f"The total wall area of your room is (totalArea)")
-
 def gallons_of_paint():
 
-gallons_of_paint= math.ceil(totalArea/square_feet_per_gallon)
+    gallons_of_paint= math.ceil(totalArea/square_feet_per_gallon)
 
+print(f"The total wall area of your room is (totalArea)")
 print(f"You need (gallons_of_paint)")
 
 
