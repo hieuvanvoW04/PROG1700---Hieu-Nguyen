a8a32231dae5f0888a31055a93fac8ddc359a092
Nguyen <W0424530@campus.nscc.ca>
2018-10-10 11:40:29 -0300

    Check PaintAssign

---
+++ In Class/PaintShop.py
@@ -0,0 +1,38 @@
+"""
+Function
+
+Name..: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+
+import math
+
+square_feet_per_gallon = 150
+
+def hello():
+    print("Welcome to the Paint Shop!")
+    print("This program will help you calculate how many cans of paint you need to buy, based on the dimensions of your room.")
+    return 1
+
+def CalculatetotalArea(x,y,z):
+    x=float(input("\nEnter the length of the room, in feet: "))
+    y=float(input("Enter the width of the room, in feet: "))
+    z=float(input("Enter the height of the room, in feet: "))
+    return (x*z*2)+(y*z*2)
+
+totalArea= CalculatetotalArea(x,y,z)
+
+print(f"The total wall area of your room is (totalArea)")
+
+def gallons_of_paint():
+
+gallons_of_paint= math.ceil(totalArea/square_feet_per_gallon)
+
+print(f"You need (gallons_of_paint)")
+
+
+
+
+    
