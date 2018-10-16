cffc1c85fb404d49472b2ab7a0386745d3d32624
Nguyen <W0424530@campus.nscc.ca>
2018-10-16 10:15:26 -0300

    RandomGame

--- In Class/GameRandom.py
+++ In Class/GameRandom.py
@@ -17,13 +17,14 @@ def main():
 
     while(number_to_guess != current_guess):
         current_guess=int(input("Guess again(1-10): "))
+        
 
-        if current_guess<number_to_guess and current_guess > 1:
+        if current_guess < number_to_guess and current_guess > 1:
             print("Your guess is too low.")
 
-        if current_guess< 1 and current_guess> 10:
+        if current_guess < 1 and current_guess > 10:
             print("Not a good guess.")
-            
+
     print("you got it")
 
 
