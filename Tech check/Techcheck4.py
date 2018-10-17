d187abc21d85adc576bea0d1c18eb3e01627dcc8
HIEU NGUYEH <43071885+hieuvanvoW04@users.noreply.github.com>
2018-10-17 01:02:08 -0300

    Prepare

---
+++ Tech check/Techcheck4.py
@@ -0,0 +1,25 @@
+"""
+Hello World
+Python Push
+Name..
+: Hieu NGuyen, Student  
+ID....: W0424530
+"""
+_AUTHOR_ = "Hieu Nguyen <W0424530@nscc.ca>"
+
+
+def main():
+
+# Input
+
+    
+
+
+# Process
+   
+   
+# Output
+   
+
+if __name__== "__main__":
+    main()
