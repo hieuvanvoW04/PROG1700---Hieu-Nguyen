0ceecd769069ded8615af3fcb24e601c60f7cd42
Nguyen <W0424530@campus.nscc.ca>
2018-10-23 10:19:37 -0300

    kk

--- In Class/Homework.Map.py
+++ In Class/Homework.Map.py
@@ -3,3 +3,4 @@
 # L=["apples","peaches","bananas"]
 # create a function that takes a word  and capitalizes any letter e's in the word
 # UUse map to apply that function to your list of fruits
+# PETER NORVIG BLOG
