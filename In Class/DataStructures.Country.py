34470e05227551faa072cfd2322dae27ca1980e2
Nguyen <W0424530@campus.nscc.ca>
2018-10-23 10:02:35 -0300

    kk

---
+++ In Class/DataStructures.Country.py
@@ -0,0 +1,27 @@
+
+# countries.append("nz")
+# c=countries[index]
+# countries[i]= "Berlins"
+# if("nz" in countries)
+# countries.insert(index,"ce")
+# countries.pop(index)
+# r=nap(func,list)
+    #print(list(r))
+# countries.xvssdf??("canada")
+# countries[1:3]= ["nz","bulgary"]
+    #del(countries[3])
+def main():
+
+    country=input("What country should I add to my list: ")
+    countries=["canada","australia","combodia","laos","china"]
+  
+    while (country in countries ):
+        print("already in list")
+        country=input("tr another one")
+
+countries.append(country)
+
+print(countries)
+
+if __name__== "__main__":
+    main()  
---
+++ In Class/Map.double.py
@@ -0,0 +1,15 @@
+def main():
+
+    
+    def double(num):
+        return(2*num)
+    list_of_nums=[1,2,3,4]
+    res=map(double,list_of_nums)
+
+    print(list(res))
+
+
+
+
+
+main()
