4f39ef7034ee48202fadc54fa0aebcee63a58a94
Nguyen <W0424530@campus.nscc.ca>
2018-10-30 10:10:53 -0300

    kk

--- In Class/301018 whileloop.py
+++ In Class/301018 whileloop.py
@@ -19,10 +19,20 @@ def whileloop():
         num=int(maybe_num)
         print(num)
 
+function
 
+# def getValidNum(num_to_check):
+#         nc=num_to_check.replace(".","")
+#         while(not num_to_check.isnumeric):
+#                 num_to_check=input("Not a number. Please try again.")
+#                 nc=num_to_check.replace(".","")
+#         else:
+#                 num_to_check=int(num_to_check)
 
+#         return(num_to_check)
 
-
+# first_int=input("Give me first #")
+# first_int=getValidNum(first.int)
 
 
 
