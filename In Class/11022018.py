3e315a51a6e917ee4c853b27dfc70e75ee3555c3
Nguyen <W0424530@campus.nscc.ca>
2018-11-02 11:34:15 -0300

    kk

---
+++ In Class/11022018.py
@@ -0,0 +1,56 @@
+
+
+# code snippet to ask user if they
+# want to continue doing something and 
+# either continue or quit
+#            keepFoing="y"
+#            while(  keepFoing="y"      ) condition: is keep going set to "y"
+#               do a bunch of stuff
+#               insert anything here
+#           keepGoing=input("Do you want to keep going(y/n)")
+
+
+# Part1: turn a snippet of code into a function
+# Part2: change a function that prints "something" to a function that returns "something"
+n=48
+for i in range(1,n+1):
+    if(n%i==0):
+        print(i)
+# n=48
+def printFactors(n):
+    for i in range(1,n+1):
+        if(n%i==0):
+            print(i)
+
+# storing result in a list
+
+def getFactorList(n):
+    FL=[]
+
+    for i in range (1,n+1):
+        if(n%i==0):
+            FL.append(i)
+    return(FL)
+
+FL1=getFactorList(12)
+FL2=getFactorList(9)
+print(FL1)
+print(FL2)
+
+
+list1=[1,3,9]
+list2=[1,2,3,4,6,12]
+def commonInLists(list1,list2):
+    commonList=[]
+    for l in list1:
+        if l in list2:
+            commonList.append(l)
+    return(commonList)
+
+
+def max(L):
+    max=L[0]
+    for i in L:
+        if(i>max):
+            max=i
+    return(max)  
