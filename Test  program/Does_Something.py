5a8f45e3776e6fc16abf9af4d5dad28d55043a54
Nguyen <W0424530@campus.nscc.ca>
2018-11-06 09:16:03 -0400

    kk

--- Test  program/Does_Something.py
+++ Test  program/Does_Something.py
@@ -1,30 +1,33 @@
 import random
 
-o = random.randint(1,25)
-la = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s', \
+random_num = random.randint(1,25)
+lowercase_alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s', \
 		  't','u','v','w','x','y','z']
-p = [' ', '.',',']
-fr = "test_file.txt"
+punc = [' ', '.',',']
+source_file = "test_file.txt"
 am = "r"
-fw = "processed.txt"
-e = "utf-8"
-om = ""
+output_file = "processed.txt"
+encoding_type = "utf-8"
+output_text = ""
 
-mf = open(fr,am)
-im = mf.read()
-
-for i in range(len(im)):
-	if im[i] in p:
-		om += im[i]
+myFile = open(source_file,am)
+input_text = myFile.read()
+# look at each character of the message
+for i in range(len(input_text)):
+	# if current char is punctuation
+	# leave it alone-pass it through
+	if input_text[i] in punc:
+		# i=i+1 => i +=1
+		output_text += output_text + input_text[i]
 	else:
-		ai = la.index(im[i].lower())
+		alpha_index = lowercase_alphabet.index(input_text[i].lower())
 		
-		ni = (ai + o) % 26
+		new_index = (alpha_index + random_num) % 26
 		
-		if im[i].upper() == im[i]:
-			om += la[ni].upper()
+		if input_text[i].upper() == input_text[i]:
+			output_text += lowercase_alphabet[ni].upper()
 		else:
-			om += la[ni]
+			output_text += lowercase_alphabet[ni]
 
-print(om)
+print(output_text)
 
